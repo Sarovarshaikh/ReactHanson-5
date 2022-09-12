fdc5d621a339bda5cf13b9839b605a2cab36ebf6
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-09-13 00:02:20 +0530

    Hanson-5

---
+++ src/Compnents/HOC.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+
+
+const productsItems = [
+   {itemName: 'Smart Tv',  startPrice: '15000 Rs'},
+   {itemName: 'Console',  startPrice: '50000 Rs'},
+   {itemName: 'Toys',  startPrice: '999 Rs'},
+   {itemName: 'Appliances',  startPrice: '25000 Rs'},
+   {itemName: 'Kitchen Ware',  startPrice: '3500 Rs'}
+]
+
+const HOC = (WrappedCompo) => {
+    class searchItem extends React.Component{
+      constructor(){
+        super();
+        this.state={
+          search: '',
+        }
+      }
+  
+      updateSearch = (e) =>{
+       this.setState({search: e.target.value})
+      }
+
+      filterItems = (e) =>{
+      return  productsItems.filter(productItems => productItems.itemName.toLowerCase().includes(this.state.search))
+      }
+      
+  render(){
+    
+    return (
+      <div>
+      
+        <label>Search</label>  
+        <input type="text" onChange={this.updateSearch}/>
+        
+      
+        <WrappedCompo products = {this.filterItems()} {...this.props}/>  
+    
+      </div>
+    )
+  }
+  } 
+  return searchItem;
+}
+export default HOC;
